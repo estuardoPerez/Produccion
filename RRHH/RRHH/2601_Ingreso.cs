044ce27c78aba2225dd7461760238340c9ba18a0
Yulio <41310771+YulioV@users.noreply.github.com>
2018-10-15 17:46:09 -0600

    RRHH

---
+++ RRHH/RRHH/2601_Ingreso.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RRHH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_cerrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Btn_minimizar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Llbl_nombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lbl_encargado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RRHH/RRHH/2606_Empaque.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace RRHH
+{
+    partial class _2606_Empaque
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_2606_Empaque));
+            this.Panel_arriba = new System.Windows.Forms.Panel();
+            this.Btn_minimizar = new System.Windows.Forms.Button();
+            this.Btn_cerrar = new System.Windows.Forms.Button();
+            this.Btn_produccion = new System.Windows.Forms.Button();
+            this.Lbl_empaque = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.Lbl_gestionFormatos = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_apellido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.Cmb_empleado = new System.Windows.Forms.ComboBox();
+            this.Cmb_tarea = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lbl_encargado = new System.Windows.Forms.Label();
+            this.txt_encargado = new System.Windows.Forms.TextBox();
+            this.Llbl_nombre = new System.Windows.Forms.Label();
+            this.cmb_maquina = new System.Windows.Forms.ComboBox();
+            this.lbl_maquina = new System.Windows.Forms.Label();
+            this.dwv_empaque = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Panel_arriba.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dwv_empaque)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Panel_arriba
+            // 
+            this.Panel_arriba.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(87)))), ((int)(((byte)(54)))));
+            this.Panel_arriba.Controls.Add(this.Btn_minimizar);
+            this.Panel_arriba.Controls.Add(this.Btn_cerrar);
+            this.Panel_arriba.Controls.Add(this.Btn_produccion);
+            this.Panel_arriba.Controls.Add(this.Lbl_empaque);
+            this.Panel_arriba.Location = new System.Drawing.Point(0, 0);
+            this.Panel_arriba.Margin = new System.Windows.Forms.Padding(4);
+            this.Panel_arriba.Name = "Panel_arriba";
+            this.Panel_arriba.Size = new System.Drawing.Size(933, 42);
+            this.Panel_arriba.TabIndex = 100;
+            // 
+            // Btn_minimizar
+            // 
+            this.Btn_minimizar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_minimizar.BackgroundImage")));
+            this.Btn_minimizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_minimizar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Btn_minimizar.FlatAppearance.BorderSize = 0;
+            this.Btn_minimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_minimizar.Location = new System.Drawing.Point(880, 0);
+            this.Btn_minimizar.Name = "Btn_minimizar";
+            this.Btn_minimizar.Size = new System.Drawing.Size(41, 41);
+            this.Btn_minimizar.TabIndex = 99;
+            this.toolTip1.SetToolTip(this.Btn_minimizar, "Minimizar");
+            this.Btn_minimizar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_cerrar
+            // 
+            this.Btn_cerrar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_cerrar.BackgroundImage")));
+            this.Btn_cerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_cerrar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Btn_cerrar.FlatAppearance.BorderSize = 0;
+            this.Btn_cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_cerrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(87)))), ((int)(((byte)(54)))));
+            this.Btn_cerrar.Location = new System.Drawing.Point(833, 0);
+            this.Btn_cerrar.Name = "Btn_cerrar";
+            this.Btn_cerrar.Size = new System.Drawing.Size(41, 41);
+            this.Btn_cerrar.TabIndex = 98;
+            this.toolTip1.SetToolTip(this.Btn_cerrar, "Cerrar");
+            this.Btn_cerrar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_produccion
+            // 
+            this.Btn_produccion.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_produccion.BackgroundImage")));
+            this.Btn_produccion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_produccion.FlatAppearance.BorderSize = 0;
+            this.Btn_produccion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_produccion.Location = new System.Drawing.Point(3, 1);
+            this.Btn_produccion.Name = "Btn_produccion";
+            this.Btn_produccion.Size = new System.Drawing.Size(41, 41);
+            this.Btn_produccion.TabIndex = 100;
+            this.Btn_produccion.UseVisualStyleBackColor = true;
+            // 
+            // Lbl_empaque
+            // 
+            this.Lbl_empaque.AutoSize = true;
+            this.Lbl_empaque.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_empaque.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Lbl_empaque.Location = new System.Drawing.Point(51, 1);
+            this.Lbl_empaque.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_empaque.Name = "Lbl_empaque";
+            this.Lbl_empaque.Size = new System.Drawing.Size(261, 39);
+            this.Lbl_empaque.TabIndex = 98;
+            this.Lbl_empaque.Text = "2606_Empaque";
+            // 
+            // Lbl_gestionFormatos
+            // 
+            this.Lbl_gestionFormatos.AutoSize = true;
+            this.Lbl_gestionFormatos.Font = new System.Drawing.Font("Century Gothic", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_gestionFormatos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Lbl_gestionFormatos.Location = new System.Drawing.Point(409, 147);
+            this.Lbl_gestionFormatos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_gestionFormatos.Name = "Lbl_gestionFormatos";
+            this.Lbl_gestionFormatos.Size = new System.Drawing.Size(116, 26);
+            this.Lbl_gestionFormatos.TabIndex = 101;
+            this.Lbl_gestionFormatos.Text = "Empaque";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 129;
+            this.label2.Tag = "";
+            this.label2.Text = "Apellido";
+            // 
+            // txt_apellido
+            // 
+            this.txt_apellido.Location = new System.Drawing.Point(200, 255);
+            this.txt_apellido.Name = "txt_apellido";
+            this.txt_apellido.ReadOnly = true;
+            this.txt_apellido.Size = new System.Drawing.Size(304, 22);
+            this.txt_apellido.TabIndex = 128;
+            this.txt_apellido.Tag = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 127;
+            this.label3.Text = "Nombre";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(200, 227);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.ReadOnly = true;
+            this.txt_nombre.Size = new System.Drawing.Size(304, 22);
+            this.txt_nombre.TabIndex = 126;
+            this.txt_nombre.Tag = "";
+            // 
+            // Cmb_empleado
+            // 
+            this.Cmb_empleado.FormattingEnabled = true;
+            this.Cmb_empleado.Location = new System.Drawing.Point(200, 197);
+            this.Cmb_empleado.Name = "Cmb_empleado";
+            this.Cmb_empleado.Size = new System.Drawing.Size(304, 24);
+            this.Cmb_empleado.TabIndex = 124;
+            // 
+            // Cmb_tarea
+            // 
+            this.Cmb_tarea.FormattingEnabled = true;
+            this.Cmb_tarea.Location = new System.Drawing.Point(200, 313);
+            this.Cmb_tarea.Name = "Cmb_tarea";
+            this.Cmb_tarea.Size = new System.Drawing.Size(304, 24);
+            this.Cmb_tarea.TabIndex = 123;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 319);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 122;
+            this.label1.Text = "Tarea designada";
+            // 
+            // Lbl_encargado
+            // 
+            this.Lbl_encargado.AutoSize = true;
+            this.Lbl_encargado.Location = new System.Drawing.Point(62, 288);
+            this.Lbl_encargado.Name = "Lbl_encargado";
+            this.Lbl_encargado.Size = new System.Drawing.Size(130, 17);
+            this.Lbl_encargado.TabIndex = 121;
+            this.Lbl_encargado.Text = "Nombre encargado";
+            // 
+            // txt_encargado
+            // 
+            this.txt_encargado.Location = new System.Drawing.Point(200, 285);
+            this.txt_encargado.Name = "txt_encargado";
+            this.txt_encargado.Size = new System.Drawing.Size(304, 22);
+            this.txt_encargado.TabIndex = 120;
+            this.txt_encargado.Tag = "";
+            // 
+            // Llbl_nombre
+            // 
+            this.Llbl_nombre.AutoSize = true;
+            this.Llbl_nombre.Location = new System.Drawing.Point(62, 202);
+            this.Llbl_nombre.Name = "Llbl_nombre";
+            this.Llbl_nombre.Size = new System.Drawing.Size(87, 17);
+            this.Llbl_nombre.TabIndex = 119;
+            this.Llbl_nombre.Text = "ID empleado";
+            // 
+            // cmb_maquina
+            // 
+            this.cmb_maquina.FormattingEnabled = true;
+            this.cmb_maquina.Location = new System.Drawing.Point(200, 343);
+            this.cmb_maquina.Name = "cmb_maquina";
+            this.cmb_maquina.Size = new System.Drawing.Size(304, 24);
+            this.cmb_maquina.TabIndex = 130;
+            // 
+            // lbl_maquina
+            // 
+            this.lbl_maquina.AutoSize = true;
+            this.lbl_maquina.Location = new System.Drawing.Point(60, 346);
+            this.lbl_maquina.Name = "lbl_maquina";
+            this.lbl_maquina.Size = new System.Drawing.Size(132, 17);
+            this.lbl_maquina.TabIndex = 131;
+            this.lbl_maquina.Text = "Maquina designada";
+            // 
+            // dwv_empaque
+            // 
+            this.dwv_empaque.AllowUserToAddRows = false;
+            this.dwv_empaque.AllowUserToDeleteRows = false;
+            this.dwv_empaque.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dwv_empaque.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dwv_empaque.Location = new System.Drawing.Point(38, 393);
+            this.dwv_empaque.Name = "dwv_empaque";
+            this.dwv_empaque.ReadOnly = true;
+            this.dwv_empaque.RowTemplate.Height = 24;
+            this.dwv_empaque.Size = new System.Drawing.Size(869, 259);
+            this.dwv_empaque.TabIndex = 132;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id empleado";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 120;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Apellido";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Nombre encargado";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 170;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Tarea designada";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 170;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Maquina designada";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 170;
+            // 
+            // _2606_Empaque
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(192)))), ((int)(((byte)(195)))));
+            this.ClientSize = new System.Drawing.Size(933, 700);
+            this.Controls.Add(this.dwv_empaque);
+            this.Controls.Add(this.lbl_maquina);
+            this.Controls.Add(this.cmb_maquina);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_apellido);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.Cmb_empleado);
+            this.Controls.Add(this.Cmb_tarea);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lbl_encargado);
+            this.Controls.Add(this.txt_encargado);
+            this.Controls.Add(this.Llbl_nombre);
+            this.Controls.Add(this.Lbl_gestionFormatos);
+            this.Controls.Add(this.Panel_arriba);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "_2606_Empaque";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "_2606_Empaque";
+            this.Panel_arriba.ResumeLayout(false);
+            this.Panel_arriba.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dwv_empaque)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Panel_arriba;
+        private System.Windows.Forms.Button Btn_minimizar;
+        private System.Windows.Forms.Button Btn_cerrar;
+        private System.Windows.Forms.Button Btn_produccion;
+        private System.Windows.Forms.Label Lbl_empaque;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label Lbl_gestionFormatos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_apellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.ComboBox Cmb_empleado;
+        private System.Windows.Forms.ComboBox Cmb_tarea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lbl_encargado;
+        private System.Windows.Forms.TextBox txt_encargado;
+        private System.Windows.Forms.Label Llbl_nombre;
+        private System.Windows.Forms.ComboBox cmb_maquina;
+        private System.Windows.Forms.Label lbl_maquina;
+        private System.Windows.Forms.DataGridView dwv_empaque;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
