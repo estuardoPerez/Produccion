adb9327fab7290f16e52388991764cc2901feecf
javiiman <39818555+javiiman@users.noreply.github.com>
2018-11-02 23:50:19 -0600

    Implosion y Explosion de Materiales (Capas)
    3ra entrega

---
+++ Implo_ExpoCapas/CapaDesign/frm_menu_prueba.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDesign
+{
+    public partial class frm_menu_prueba : Form
+    {
+        public frm_menu_prueba()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_explosionMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_explosionSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Implosion_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frm_implosion frm_implosion1 = new frm_implosion();
+            frm_implosion1.Show();
+        }
+
+        private void btn_Explosion_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frm_explosion frm_explosion1 = new frm_explosion();
+            frm_explosion1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frm_peticion_materia frm_peticion1 = new frm_peticion_materia();
+            frm_peticion1.Show();
+        }
+    }
+}
---
+++ Implo_ExpoCapas/CapaDesign/frm_peticion_materia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDesign
+{
+    public partial class frm_peticion_materia : Form
+    {
+        public frm_peticion_materia()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_explosionMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_explosionSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
