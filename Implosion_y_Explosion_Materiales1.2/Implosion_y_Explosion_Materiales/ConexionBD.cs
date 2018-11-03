5b56186bb2d7de2e7f10a7740cf023f52c2495bd
javiiman <39818555+javiiman@users.noreply.github.com>
2018-11-02 23:47:33 -0600

    Implosion y Explosion de Materiales
    3ra entrega

---
+++ Implosion_y_Explosion_Materiales1.2/Implosion_y_Explosion_Materiales/ConexionBD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace Implosion_y_Explosion_Materiales
+{
+    class ConexionBD
+    {
+        static OdbcConnection cnx = null;
+        public static OdbcConnection getDB()
+        {
+            cnx = new OdbcConnection(Properties.Settings.Default.ruta);
+            cnx.Open();
+            return cnx;
+        }
+
+
+    }
+}
---
+++ Implosion_y_Explosion_Materiales1.2/Implosion_y_Explosion_Materiales/frm_menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Implosion_y_Explosion_Materiales
+{
+    public partial class Menu_prueba : Form
+    {
+        public Menu_prueba()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_explosionMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_explosionSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frm_Implosion frm_implosion1 = new frm_Implosion();
+            frm_implosion1.Show();
+        }
+
+        private void btn_Explosion_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frm_Explosion frm_explosion1 = new frm_Explosion();
+            frm_explosion1.Show();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+             frm_peticion_Materia frm_peticion = new frm_peticion_Materia();
+            frm_peticion.Show();
+        }
+    }
+}
