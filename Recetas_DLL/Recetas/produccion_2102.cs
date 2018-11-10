450ac1bd860974a961a0221295f76b751e11536a
josueponciano <poncianojosue@hotmail.com>
2018-11-09 23:44:48 -0600

    Revert "Recetas_DLL"
    This reverts commit 9448537dd9fc2bbbd78a93ea6e63d1fc2604c8f0.

--- Recetas_DLL/Recetas/produccion_2102.cs
+++ Recetas_DLL/Recetas/produccion_2102.cs
@@ -21,57 +21,33 @@ namespace Recetas
 
         private void btn_salir_Click(object sender, EventArgs e)
         {
-            /* 
-             * programador: Josue Roberto Ponciaco Del Cid
-             * descripcion: cierre de formulario
-             */
-            if (anterior != null)
+            if(anterior != null)
                 anterior.Show();
             this.Close();
         }
 
         private void btn_minimizar_Click(object sender, EventArgs e)
         {
-            /* 
-             * programador: Josue Roberto Ponciaco Del Cid
-             * descripcion: minimiza el formulario
-             */
             this.WindowState = FormWindowState.Minimized;
         }
 
         private void produccion_2102_HelpRequested(object sender, HelpEventArgs hlpevent)
         {
-            /* 
-             * programador: Josue Roberto Ponciaco Del Cid
-             * descripcion: obtiene HTML de ayuda
-             */
             Help.ShowHelp(this, "C:/Users/chopes/Documents/Visual Studio 2015/Projects/Recetas_DLL/ayuda/ayuda.chm", "nueva_receta.html");
         }
 
         private void lbl_costo_total_Click(object sender, EventArgs e)
         {
-            /* 
-             * programador: Josue Roberto Ponciaco Del Cid
-             * descripcion: 
-             */
 
         }
 
         private void btn_eliminar_campo_Click(object sender, EventArgs e)
         {
-            /* 
-             * programador: Josue Roberto Ponciaco Del Cid
-             * descripcion: 
-             */
 
         }
 
         private void btn_agregar_campo_Click(object sender, EventArgs e)
         {
-            /* 
-             * programador: Josue Roberto Ponciaco Del Cid
-             * descripcion: 
-             */
 
         }
     }
